f59363f0d39957b375c1bdca7ee7a2a9b5f5ecef
rahulvaish <rahulvaish@Rahuls-MacBook-Pro.local>
2023-01-02 16:02:08 -0500

    SB Inter communication

--- CalculatorServiceClient/src/main/java/com/calculator/CalculatorController.java
+++ CalculatorServiceClient/src/main/java/com/calculator/CalculatorController.java
@@ -1,10 +1,7 @@
 package com.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.calculator.model.Number;
 
@@ -19,45 +16,10 @@ public class CalculatorController {
 		return "Calculator Client Application";
 	}
 
-	@RequestMapping(value = "/addition/float/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String floatAddition(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/addition/float/");
+	@PostMapping("/addition/")
+	public Number addTwoNumbers(@RequestBody Number number) {
+		Number num = pseudoServiceForCalculatorService.callService(number);
+		return num;
 	}
 
-	@RequestMapping(value = "/substraction/float/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String floatSubstraction(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/substraction/float/");
-	}
-
-	@RequestMapping(value = "/multiplication/float/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String floatMultiplication(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/multiplication/float/");
-	}
-
-	@RequestMapping(value = "/division/float/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String floatDivision(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/division/float/");
-	}
-	
-	// -----------------------------------
-
-	@RequestMapping(value = "/addition/int/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String intAddition(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/addition/int/");
-	}
-
-	@RequestMapping(value = "/substraction/int/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String intSubstraction(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/substraction/int/");
-	}
-
-	@RequestMapping(value = "/multiplication/int/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String intMultiplication(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/multiplication/int/");
-	}
-
-	@RequestMapping(value = "/division/int/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public String intDivision(@RequestBody Number number) {
-		return pseudoServiceForCalculatorService.callService(number.getFirstNumber(), number.getSecondNumber(),"/division/int/");
-	}
 }
